e776cc7db8503b6aa2cc331af360a691d5f1788e
Kim-Du-Yeon <91865644+Kim-Du-Yeon@users.noreply.github.com>
2021-11-13 14:30:17 +0900

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,18 +1,18 @@
-def add(a, b):
+def add(a, b):       # 더하기
     num=a+b
     return num
 
-def minus(a, b):
+def minus(a, b):     # 빼기
     if a>b:
         num=a-b
     else :
         num=b-a
     return num
 
-def divide(a, b):
+def divide(a, b):    # 곱하기
     num=a/b
     return num
 
-def multiply(a, b):
+def multiply(a, b):  # 나누기
     num=a*b
     return num
